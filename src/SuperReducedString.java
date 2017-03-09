fbb5c8e9695825bcefa9ba1ddfd5157031733a9b
Prachi Bhopatkar <bhopatkarprachi@gmail.com>
2017-03-09 00:46:44 -0500

    Update SuperReducedString.java

--- src/SuperReducedString.java
+++ src/SuperReducedString.java
@@ -1,3 +1,14 @@
+/*
+Steve has a string, , consisting of  lowercase English alphabetic letters.
+In one operation, he can delete any pair of adjacent letters with same value. 
+For example, string "aabcc" would become either "aab" or "bcc" after operation.
+
+Steve wants to reduce  as much as possible.
+To do this, he will repeat the above operation as many times as it can be performed. 
+Help Steve out by finding and printing 's non-reducible form!
+
+Note: If the final string is empty, print Empty String .
+*/
 import java.util.*;
 public class SuperReducedString {
 	public static Stack reduceString(String str){
